5d868725141078fc77e3316fadb60a1fe69fe53f
nitbooz <88794824+nitbooz@users.noreply.github.com>
2025-04-02 12:38:32 +0200

    Add files via upload

--- custom_components/scorta_pellet/config_flow.py
+++ custom_components/scorta_pellet/config_flow.py
@@ -2,10 +2,12 @@
 from __future__ import annotations
 
 from typing import Any
+import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
+from homeassistant.helpers import config_validation as cv
 
 class ScortaPelletConfigFlow(config_entries.ConfigFlow, domain="scorta_pellet"):
     """Handle a config flow for Scorta Pellet."""
@@ -17,8 +19,6 @@ class ScortaPelletConfigFlow(config_entries.ConfigFlow, domain="scorta_pellet"):
         user_input: dict[str, Any] | None = None,
     ) -> FlowResult:
         """Handle the initial step."""
-        import voluptuous as vol
-        from homeassistant.helpers import config_validation as cv
         
         if user_input is None:
             return self.async_show_form(
